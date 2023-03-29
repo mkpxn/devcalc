767ab47ff7b1ddea98f492afdc39f27c7aa252da
CBRA-Mirko <pixnermirko@gmail.com>
2023-03-29 18:20:37 +0200

    add colors

--- main.py
+++ main.py
@@ -7,13 +7,23 @@ class GUI:
     font_m = ('Arial', 14)
     font_s = ('Arial', 10)
 
+    # Colors
+    color_bg        = '#010409'
+    color_primary   = '#0D1117'
+    color_accent    = '#4CCEAC'
+    color_grey_f    = '#7A8490'     # Grey / Font
+    color_grey_b    = '#21262D'     # Grey / Button
+    color_white     = '#E4E8EC'
+    color_red       = '#DB4F4A'
+    color_blue      = '#6870FA'
+
     def __init__(self):
 
         # Root / Window
         self.root = tk.Tk()
         self.root.title('Dev Calculator')
         self.root.geometry('350x400')
-        self.root.configure()
+        self.root.configure(background = self.color_bg)
 
         # Validation
         def validate(input):
@@ -25,7 +35,7 @@ class GUI:
         int_validation = self.root.register(validate)
 
         # Frame 1: Pixel-to-REM calculator
-        self.remcalc_frame = tk.Frame(self.root)
+        self.remcalc_frame = tk.Frame(self.root, background = self.color_bg)
         # self.remcalc_frame.columnconfigure(0, weight = 1)
         # self.remcalc_frame.columnconfigure(1, weight = 1)
         # self.remcalc_frame.columnconfigure(2, weight = 1)
@@ -37,11 +47,12 @@ class GUI:
             self.remcalc_frame, 
             text = "Rem Calculator", 
             font = self.font_l,
+            bg = self.color_bg,
+            fg = self.color_accent,
         )
         self.remcalc_label.grid(
             row = 0, 
             column = 0, 
-            # columnspan = 5, 
             pady=(0, 20),
         )
 
@@ -50,6 +61,8 @@ class GUI:
             self.remcalc_frame,
             text = "Pixel",
             font = self.font_s,
+            bg = self.color_bg,
+            fg = self.color_white,
         )
         self.remcalc_px_label.grid(
             row = 1, 
@@ -61,6 +74,9 @@ class GUI:
             self.remcalc_frame,
             validate = 'all',
             validatecommand = (int_validation, '%P'),
+            bg = self.color_primary,
+            fg = self.color_white,
+            insertbackground = self.color_white,
         )
         self.remcalc_px.insert(0, 16)
         self.remcalc_px.bind("<KeyPress>", self.shortcut)
@@ -70,23 +86,13 @@ class GUI:
         )
         self.remcalc_px.focus()
 
-        # Frame 1: and 
-        # self.remcalc_and = tk.Label(
-        #     self.remcalc_frame,
-        #     text = '=',
-        #     font = ('Arial', 14),
-        # )
-        # self.remcalc_and.grid(
-        #     row = 3, 
-        #     column = 0, 
-        #     padx = 20,
-        # )
-
         # Frame 1: Label for root input
         self.remcalc_root_label = tk.Label(
             self.remcalc_frame,
             text = "Root",
             font = self.font_s,
+            bg = self.color_bg,
+            fg = self.color_white,
         )
         self.remcalc_root_label.grid(
             row = 3, 
@@ -98,7 +104,10 @@ class GUI:
         self.remcalc_root = tk.Entry(
             self.remcalc_frame,
             validate = 'all',
-            validatecommand = (int_validation, '%P')
+            validatecommand = (int_validation, '%P'),
+            bg = self.color_primary,
+            fg = self.color_white,
+            insertbackground = self.color_white,
         )
         self.remcalc_root.insert(0, 16)
         self.remcalc_root.grid(
@@ -112,6 +121,8 @@ class GUI:
             text = "berechnen",
             font = self.font_m,
             command = self.remcalc,
+            bg = self.color_primary,
+            fg = self.color_accent,
         )
         self.remcalc_button.grid(
             row = 5, 
@@ -124,11 +135,12 @@ class GUI:
             self.remcalc_frame,
             text = "1.0",
             font = self.font_l,
+            bg = self.color_bg,
+            fg = self.color_accent,
         )
         self.remcalc_result.grid(
             row = 6, 
             column = 0, 
-            # columnspan = 5, 
             pady = 30,
         )
 
